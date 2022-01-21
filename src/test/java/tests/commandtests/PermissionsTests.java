804f4e0c43bc7d3bcca9b4a1c05c9dbfad5aa802
Benlewis9000 <benjaminlewis9000@gmail.com>
2022-01-21 00:59:24 +0000

    Permissions tests.

---
+++ src/test/java/tests/commandtests/PermissionsTests.java
@@ -0,0 +1,40 @@
+package tests.commandtests;
+
+import be.seeseemelk.mockbukkit.entity.PlayerMock;
+import io.benlewis.tagminigame.game.tag.TagGame;
+import org.bukkit.entity.Player;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import tests.MockBukkitTests;
+
+import java.util.Locale;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PermissionsTests extends MockBukkitTests {
+
+    // TODO: Permission defaults unintentionally ignored here - perhaps a MockBukkit bug?
+    @ParameterizedTest
+    @MethodSource("permissions")
+    void permissions_noPermThenPerm(String command, String permission){
+        PlayerMock p = server.addPlayer();
+        server.dispatchCommand(p, command);
+        assertTrue(p.nextMessage().contains("do not have permission"));
+        p.addAttachment(plugin).setPermission(permission, true);
+        server.dispatchCommand(p, command);
+        assertFalse(p.nextMessage().toLowerCase(Locale.UK).contains("do not have permission"));
+    }
+
+    private static Stream<Arguments> permissions() {
+        return Stream.of(
+                Arguments.of("create", "tag.create"),
+                Arguments.of("join", "tag.join"),
+                Arguments.of("quit", "tag.quit"),
+                Arguments.of("settag", "tag.settag")
+        );
+    }
+
+}
