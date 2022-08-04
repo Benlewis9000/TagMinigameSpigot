ac0d643bc0d9bdbe3f8ea0071b85e2e2d6ddb0ff
benlewis9000 <benjaminlewis9000@gmail.com>
2022-08-04 23:10:07 +0100

    Implement IConfigManager.

--- src/main/java/io/benlewis/tagminigame/configuration/ConfigManager.java
+++ src/main/java/io/benlewis/tagminigame/configuration/ConfigManager.java
@@ -1,4 +1,50 @@
 package io.benlewis.tagminigame.configuration;
 
-public class ConfigManager {
+import io.benlewis.tagminigame.TagPlugin;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class ConfigManager implements IConfigManager {
+
+    private final TagPlugin plugin;
+    private HashMap<ConfigType, FileConfiguration> configurations;
+
+    public ConfigManager(TagPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public FileConfiguration get(ConfigType type) {
+        if (configurations.containsKey(type)) {
+            return configurations.get(type);
+        }
+
+        File file = new File(plugin.getDataFolder(), type.fileName);
+        if (!file.exists()){
+            file.getParentFile().mkdirs();
+            plugin.saveResource(type.fileName, false);
+        }
+
+        FileConfiguration config = new YamlConfiguration();
+        try {
+            config.load(file);
+        }
+        catch (InvalidConfigurationException ex){
+            plugin.getLogger().warning("Invalid configuration for file \"%s\". Using defaults.".formatted(type.fileName));
+            ex.printStackTrace();
+        }
+        catch (IOException ex){
+            plugin.getLogger().warning("Unable to read configuration file \"%s\". Using defaults.".formatted(type.fileName));
+            ex.printStackTrace();
+        }
+        // TODO defaults for files not loaded - this will probs be done in whatever class processes the FileConfiguration
+        configurations.put(type, config);
+        return config;
+    }
+
 }
